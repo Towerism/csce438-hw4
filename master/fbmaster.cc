cd82b32bb28a3bcefeb60237d029f81cebe23d8f
Afisher21 <crazyfisher21@gmail.com>
2017-04-05 22:46:02 -0500

    Purpose for master added

--- master/fbmaster.cc
+++ master/fbmaster.cc
@@ -1,5 +1,12 @@
 #include <iostream>
-
+/*
+	purpose: 
+		1) Listen and wait for connection
+		2) Connect connecting client to a waiting worker (provide address and port)
+			* Load balance 
+		3) Provide backup worker information to client
+		4) Disconnect
+*/
 int main() {
   std::cout << "Hi authtin :) \n";
 }
