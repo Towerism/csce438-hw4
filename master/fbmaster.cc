1fe16cae44b6eb310b46eb9aa61884be984d760e
crazyfisher21 <crazyfisher21@tamu.edu>
2017-04-08 18:19:01 -0500

    master allocates replacements when a crash occurs (attmept 3)

--- master/fbmaster.cc
+++ master/fbmaster.cc
@@ -177,11 +177,11 @@ class MasterServiceImpl final : public MasterServer::Service{
 			}
 		}
 		else{
-			WorkerProcess wi = select_randomly(backupWorkers);
+			WorkerProcess wi = select_randomly(backupWorkers.start(), backupWorkers.end());
 			wi.pipe->Write(instruction);
 			// Tell workers a replacement link
 			for(auto worker:workerThreads){
-				WorkerProcess wp = select_randomly(backupWorkers);
+				WorkerProcess wp = select_randomly(backupWorkers.start(), backupWorkers.end());
 				MessageInfo mi;
 				WorkerInfo wi;
 				wi.set_client_count(wp.clientsConnected);
