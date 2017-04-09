1ae995a143ba342af64309970f76c0f9064a5803
crazyfisher21 <crazyfisher21@tamu.edu>
2017-04-08 22:32:22 -0500

    master channel added to worker thread (reformated MasterChannel to try to fix undefined references)

--- server/MasterChannel.h
+++ server/MasterChannel.h
@@ -18,10 +18,12 @@ class MasterChannel{
 	// Send new information to master
 	bool sendCommand(hw2::ServerInfo&);
 	MasterChannel(hw2::WorkerInfo wi, std::shared_ptr<grpc::ChannelInterface> channel):myInfo( wi),stub(hw2::MasterServer::NewStub(channel)){
+		/*
 		CommandChat();
 		hw2::ServerInfo si;
 		si.set_allocated_worker(&myInfo);
 		si.set_message_type(hw2::ServerInfo::REGISTER);
 		sendCommand(si);
+		*/
 		}
 };
