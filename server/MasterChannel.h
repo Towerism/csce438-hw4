0fbddd1e70a2455ee2c4c92766015ff64c6cabe2
crazyfisher21 <crazyfisher21@tamu.edu>
2017-04-08 22:03:22 -0500

    master channel added to worker thread (reformated MasterChannel to try to fix undefined references)

--- server/MasterChannel.h
+++ server/MasterChannel.h
@@ -3,7 +3,20 @@
 #include <mutex>
 #include <condition_variable>
 class MasterChannel{
+	private:
+	// Establish connection to master
+	bool CommandChat();
+	hw2::WorkerInfo myInfo;
+	std::unique_ptr<hw2::MasterServer::Stub> stub;
+	std::mutex sendMutex;
+	std::condition_variable cvMutex;
+	bool ready = false;
+	hw2::ServerInfo outMessage;
+	
+	
 	public:
+	// Send new information to master
+	bool sendCommand(hw2::ServerInfo&);
 	MasterChannel(hw2::WorkerInfo wi, std::shared_ptr<grpc::ChannelInterface> channel):myInfo( wi),stub(hw2::MasterServer::NewStub(channel)){
 		CommandChat();
 		hw2::ServerInfo si;
@@ -11,16 +24,4 @@ class MasterChannel{
 		si.set_message_type(hw2::ServerInfo::REGISTER);
 		sendCommand(si);
 		}
-	// Establish connection to master
-	bool CommandChat();
-	// Send new information to master
-	bool sendCommand(hw2::ServerInfo&);
-	
-	private:
-	hw2::WorkerInfo myInfo;
-	std::unique_ptr<hw2::MasterServer::Stub> stub;
-	std::mutex sendMutex;
-	std::condition_variable cvMutex;
-	bool ready = false;
-	hw2::ServerInfo outMessage;
 };
