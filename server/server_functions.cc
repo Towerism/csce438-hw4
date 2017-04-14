328887989f74c09051bc18a5bc6490d449ea1462
Martin Fracker <martin.frackerjr@gmail.com>
2017-04-14 14:03:55 -0500

    Add file locking

--- server/server_functions.cc
+++ server/server_functions.cc
@@ -1,4 +1,5 @@
 #include "server_functions.h"
+#include "file_locking.h"
 
 #define USER_FOLDER "users/"
 #define ALL_USERS_LIST "all_users.txt"
@@ -6,39 +7,8 @@
 #define FOLLOWED_BY_LIST "_followed_by_list.txt"
 #define NEW_MESSAGE "_new_messages.txt"
 
-
-/// <Title>  readFile   </Title>
-/// <Purpose> Opens a file and returns a string vector of the contents </Purpose>
-/// <Inputs>
-/// 	<Param>
-///			<Title> filename </Title>
-///			<Value> Name of file to be opened and read </Value>
-///		</Param>
-///		<Param>
-///			<Title> result </Title>
-///			<Value> Empty string vector </Value>
-///			<Output> List of all contents in file </Output>
-///		</Param>
-///	</Inputs>
-/// <Output>
-///		<Param>
-///			<Title> Success </Title>
-/// 		<Value> 0 </Value>
-///		</Param>
-///		<Param>
-///			<Title> Generic Failure </Title>
-///			<Value>  - 1 </Value>
-///		</Param>
-///		<Param>
-///			<Title> File does not exist </Title>
-///			<Value>  - 2 </Value>
-///		</Param>
-///		<Param>
-///			<Title> File could not be read </Title>
-///			<Value>  - 2 </Value>
-///		</Param>
-/// </Output>
 int readFile(string filename, vector<string> &result){
+  lockFile(filename);
 	//if( access( filename.c_str(), F_OK ) == -1 ){
 	//	return -2;
 	//}
@@ -52,45 +22,13 @@ int readFile(string filename, vector<string> &result){
 		result.push_back(line);
 	}
 	file.close();
+  unlockFile(filename);
 	return 0;
 }
 
-
-
-/// <Title>  writeFile   </Title>
-/// <Purpose> Opens a file and writes the new message to it </Purpose>
-/// <Inputs>
-/// 	<Param>
-///			<Title> filename </Title>
-///			<Value> Name of file to be opened and written to </Value>
-///		</Param>
-///		<Param>
-///			<Title> input </Title>
-///			<Value> String vector with contents to write </Value>
-///			<Output> List of all contents in file </Output>
-///		</Param>
-///		<Param>
-///			<Title> overwrite </Title>
-///			<Value> True: replace contents of file with input</Value>
-///			<Value> False: Append input to file </Value>
-///		</Param>
-///	</Inputs>
-/// <Output>
-///		<Param>
-///			<Title> Success </Title>
-/// 		<Value> 0 </Value>
-///		</Param>
-///		<Param>
-///			<Title> Generic Failure </Title>
-///			<Value>  - 1 </Value>
-///		</Param>
-///		<Param>
-///			<Title> File could not be read </Title>
-///			<Value>  - 2 </Value>
-///		</Param>
-/// </Output>
 int writeFile(string filename, vector<string> &input, bool overwrite){
 	ofstream file;
+  lockFile(filename);
 	// Check if file exists. If it does not, it must be opened in 
 	if( access( filename.c_str(), F_OK ) == -1 ){
 		file.open(filename);
@@ -108,38 +46,10 @@ int writeFile(string filename, vector<string> &input, bool overwrite){
 		file << line << "\n";
 	}
 	file.close();
+  unlockFile(filename);
 	return 0;
 }
 
-
-/// <Title>  listCommand   </Title>
-/// <Purpose> Performs required functionality of LIST command </Purpose>
-/// <Inputs>
-/// 	<Param>
-///			<Title> client </Title>
-///			<Value> User name of client requesting the list </Value>
-///		</Param>
-///		<Param>
-///			<Title> allUsers </Title>
-///			<Value> Empty string vector </Value>
-///			<Output> List of all users for FB_clone </Output>
-///		</Param>
-///		<Param>
-///			<Title> clientFollowing </Title>
-///			<Value> Empty string vector </Value>
-///			<Output> List of all users that "client" is already following </Output>
-///		</Param>
-///	</Inputs>
-/// <Output>
-///		<Param>
-///			<Title> Success </Title>
-/// 		<Value> 0 </Value>
-///		</Param>
-///		<Param>
-///			<Title> Failure </Title>
-///			<Value> - 1 </Value>
-///		</Param>
-/// </Output>
 int listCommand(string client, vector<string> &allUsers, vector<string> &clientFollowing){
 	if(readFile(string(USER_FOLDER) + string(ALL_USERS_LIST), allUsers) != 0){
 		// All users file is missing, or was unable to be opened for some reason
@@ -152,24 +62,6 @@ int listCommand(string client, vector<string> &allUsers, vector<string> &clientF
 	return 0;
 }
 
-/// <Title>  registerUser  </Title>
-/// <Purpose> Verifies the user is already in the database. If not, initializes their account </Purpose>
-/// <Inputs>
-/// 	<Param>
-///			<Title> client </Title>
-///			<Value> User name of client requesting the list </Value>
-///		</Param>
-///	</Inputs>
-/// <Output>
-///		<Param>
-///			<Title> Success </Title>
-/// 		<Value> 0 </Value>
-///		</Param>
-///		<Param>
-///			<Title> Failure </Title>
-///			<Value> - 1 </Value>
-///		</Param>
-/// </Output>
 int registerUser(const string client){
 	vector<string> input;
 	if(readFile(string(USER_FOLDER) + client + string(NEW_MESSAGE), input) != 0){
@@ -187,40 +79,6 @@ int registerUser(const string client){
 	return 0;
 }
 
-
-
-
-
-
-/// <Title>  postMessage  </Title>
-/// <Purpose> Post a message to the users chatroom </Purpose>
-/// <Inputs>
-/// 	<Param>
-///			<Title> client </Title>
-///			<Value> User name of client sending message </Value>
-///		</Param>
-///		<Param>
-///			<Title> message </Title>
-///			<Value> Message to be posted </Value>
-///		</Param>
-///	</Inputs>
-/// <Output>
-///		<Param>
-///			<Title> Success </Title>
-/// 		<Value> 0 </Value>
-///		</Param>
-///		<Param>
-///			<Title> Missing followed_by_list file </Title>
-///			<Value> 1 </Value>
-///		<Param>
-///			<Title> Failure </Title>
-///			<Value> 2 </Value>
-///		</Param>
-///		<Param>
-///			<Title> No message to post </Title>
-///			<Value> 3 </Value>
-///		</Param>
-/// </Output>
 int postMessage(string client, string message){
 	// Add message to New-Messages file for all of the users followers
 	if(message.size() == 0){
@@ -243,37 +101,6 @@ int postMessage(string client, string message){
 	}
 	return 0;
 }
-
-// <Title>  removeFromFile  </Title>
-/// <Purpose> helper function for leaveUser. Removes an undesired line from a file </Purpose>
-/// <Inputs>
-/// 	<Param>
-///			<Title> fileUse </Title>
-///			<Value> file extension (_following_list.txt) </Value>
-///		</Param>
-///		<Param>
-///			<Title> removeFrom </Title>
-///			<Value> The username that data is being removed from  </Value>
-///		</Param>
-///		<Param>
-///			<Title> removeName </Title>
-///			<Value> the line (username) to be removed </Value>
-///		</Param>
-///	</Inputs>
-/// <Output>
-///		<Param>
-///			<Title> Success </Title>
-/// 		<Value> 0 </Value>
-///		</Param>
-///		<Param>
-///			<Title> Client not following user to begin with </Title>
-///			<Value> 1 </Value>
-///		<Param>
-///			<Title> Failure </Title>
-///			<Value> - 1 </Value>
-///		</Param>
-/// </Output>
-
 
 int removeFromFile(string fileUse, string removeFrom, string removeName, bool isMessages){
 	vector<string> clientFollowing;
@@ -307,36 +134,6 @@ int removeFromFile(string fileUse, string removeFrom, string removeName, bool is
 
 }
 
-// <Title>  leaveUser  </Title>
-/// <Purpose> Unsubscribe CLIENT from USER </Purpose>
-/// <Inputs>
-/// 	<Param>
-///			<Title> client </Title>
-///			<Value> User name of client requesting to unfriend someone </Value>
-///		</Param>
-///		<Param>
-///			<Title> user </Title>
-///			<Value> The username of the friend to leave  </Value>
-///		</Param>
-///	</Inputs>
-/// <Output>
-///		<Param>
-///			<Title> Success </Title>
-/// 		<Value> 0 </Value>
-///		</Param>
-///		<Param>
-///			<Title> Client not following user to begin with </Title>
-///			<Value> 1 </Value>
-///		<Param>
-///			<Title> Failure </Title>
-///			<Value> 2 </Value>
-///		</Param>
-///		<Param>
-///			<Title> Failed to remove from Messages </Title>
-///			<Value> 3 </Value>
-///		</Param>
-/// </Output>
-
 int leaveUser(string client, string user){
 	// Remove USER from CLIENT following list
 	if(	removeFromFile(string(FOLLOWING_LIST), client, user) != 0){
@@ -355,39 +152,6 @@ int leaveUser(string client, string user){
 	return 0;
 }
 
-
-
-
-
-// <Title>  checkRecent  </Title>
-/// <Purpose> Return any new messages to the user </Purpose>
-/// <Inputs>
-/// 	<Param>
-///			<Title> client </Title>
-///			<Value> User name of client requesting new messages </Value>
-///		</Param>
-///		<Param>
-///			<Title> lastReceived </Title>
-///			<Value> The last string the client received  </Value>
-///		</Param>
-///		<Param>
-///			<Title> newMessages </Title>
-///			<Value> Empty string vector to bind new messages to </Value>
-///		</Param>
-///	</Inputs>
-/// <Output>
-///		<Param>
-///			<Title> Success </Title>
-/// 		<Value> 0 </Value>
-///		</Param>
-///		<Param>
-///			<Title>  </Title>
-///			<Value> 1 </Value>
-///		<Param>
-///			<Title> Failure </Title>
-///			<Value> 2 </Value>
-///		</Param>
-/// </Output>
 int checkRecent(string client, string lastReceived, vector<string> &newMessages){
 	if(lastReceived != ""){
 		lastReceived.erase(0,1); // Remove the prepended newLine character, courtesy of gRPC
@@ -421,50 +185,6 @@ int checkRecent(string client, string lastReceived, vector<string> &newMessages)
 	}
 	return 0;
 }
-
-
-
-/// <Title>  Join  </Title>
-/// <Purpose> Join a different users page (follow them)  </Purpose>
-/// <Inputs>
-/// 	<Param>
-///			<Title> client </Title>
-///			<Value> User name of client sending message </Value>
-///		</Param>
-///		<Param>
-///			<Title> friend </Title>
-///			<Value> Name of the new user client wants to follow  </Value>
-///		</Param>
-///	</Inputs>
-/// <Output>
-///		<Param>
-///			<Title> Success </Title>
-/// 		<Value> 0 </Value>
-///		</Param>
-///     <Param>
-///			<Title> Empty input </Title>
-//			<value 3
-///		</Param>
-///		<Param>
-///			<Title> Friend Not Found </Title>
-///			<Value> 1 </Value>
-///		<Param>
-///			<Title> Failure </Title>
-///			<Value> 2 </Value>
-///		</Param>
-////		<Param>
-///			<Title> Invalid arguments</Title>
-///			<Value> 3 </Value>
-///		</Param>
-///		<Param>
-///			<Title> client already follows user </Title>
-///			<Value> 4 </Value>
-///		</Param>
-///		<Param>
-///			<Title> Client trying to follow themselves </Title>
-///			<Value> 5 </Value>
-///		</Param>
-// </Output>
 
 int joinFriend(string client, string user){
 	if(client == "" || user == ""){
