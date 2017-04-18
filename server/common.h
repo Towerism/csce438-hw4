54c45ad5d00fd80219f8edc38daa96ad5f9b1c3c
Martin Fracker <martin.frackerjr@gmail.com>
2017-04-18 14:55:52 -0500

    Move debug definition into makefile

--- server/common.h
+++ server/common.h
@@ -1,6 +1,5 @@
 #ifndef FACEBOOK_CLONE_SERVER_HEADER_GUARD
 #define FACEBOOK_CLONE_SERVER_HEADER_GUARD
-#define DEBUG
 #include <master.grpc.pb.h>
 #include <google/protobuf/timestamp.pb.h>
 #include <google/protobuf/duration.pb.h>
